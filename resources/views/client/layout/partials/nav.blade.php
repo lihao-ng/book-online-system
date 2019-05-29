94671fa7cc3ff80aa6c0a6c1837706266bfb3f04
ernchenghoo <ernchenghoo2@gmail.com>
2019-05-29 19:32:19 +0800

    incomplete homepage

--- resources/views/client/layout/partials/nav.blade.php
+++ resources/views/client/layout/partials/nav.blade.php
@@ -1,1 +1,35 @@
-Insert Client nav blade here
+<div class="navbar navbar-expand-lg home-nav-bar">
+    <a class="navbar-brand" href="#">Bookie</a>
+    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="glyphicon glyphicon-align-justify nav-hamburger"></span>
+    </button> -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <i class="fa fa-bars" aria-hidden="true"></i>
+  </button>
+    <div id="navbarNavDropdown" class="navbar-collapse collapse">
+        <ul class="navbar-nav mr-auto">                      
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown nav-bar-elements">
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-ite" href="#">Another action</a>
+                </div>
+            </li>
+            <li class="nav-item nav-bar-elements">
+                <a class="nav-link" href="{{ url('/login') }}">Login</a>
+            </li>
+            <li class="nav-item nav-bar-elements">
+                <a class="nav-link" href="{{ url('/register') }}">Register</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+         <li class="nav-item">
+                <a class="nav-link" href="{{ url('/register') }}">Cart</a>
+          </li>
+        </ul>
+    </div>
+</div>
--- resources/views/client/pages/home.blade.php
+++ resources/views/client/pages/home.blade.php
@@ -1,90 +1,189 @@
 @extends('client.layout.master')
 
-@section('styles')
-<style>
-  html, body {
-    background-color: #fff;
-    color: #636b6f;
-    font-family: 'Nunito', sans-serif;
-    font-weight: 200;
-    height: 100vh;
-    margin: 0;
-  }
+@section('content')
 
-  .full-height {
-    height: 100vh;
-  }
-
-  .flex-center {
-    align-items: center;
-    display: flex;
-    justify-content: center;
-  }
-
-  .position-ref {
-    position: relative;
-  }
-
-  .top-right {
-    position: absolute;
-    right: 10px;
-    top: 18px;
-  }
-
-  .content {
-    text-align: center;
-  }
-
-  .title {
-    font-size: 84px;
-  }
-
-  .links > a {
-    color: #636b6f;
-    padding: 0 25px;
-    font-size: 13px;
-    font-weight: 600;
-    letter-spacing: .1rem;
-    text-decoration: none;
-    text-transform: uppercase;
-  }
-
-  .m-b-md {
-    margin-bottom: 30px;
-  }
-</style>
-@endsection
-
-</head>
-<body>
-  <div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-    <div class="top-right links">
-      @auth
-      <a href="{{ url('/home') }}">Home</a>
-      @else
-      <a href="{{ route('login') }}">Login</a>
-
-      @if (Route::has('register'))
-      <a href="{{ route('register') }}">Register</a>
-      @endif
-      @endauth
+<!------------Carousel------------------------------------------------------->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100 carousel-image" img src="images/book1.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">			    
+			    <p class="carousel-subtitle">Reading is the best gift of humanity</p>
+			    <h5 class="carousel-title">Keep Reading</h5>
+			    <button type="button" class="btn btn-dark text-light carousel-button">See more</button>
+			</div>
+		</div>			
+    <div class="carousel-item">
+      <img class="d-block w-100 carousel-image" img src="images/book2.jpg" alt="Second slide">
+      <div class="carousel-caption d-none d-md-block">			    
+			    <p class="carousel-subtitle">Reading is the best gift of humanity</p>
+			    <h5 class="carousel-title">Keep Reading</h5>
+			    <button type="button" class="btn btn-dark text-light carousel-button">See more</button>
+			</div>
     </div>
-    @endif
-
-    <div class="content">
-      <div class="title m-b-md">
-        Laravel
-      </div>
-
-      <div class="links">
-        <a href="https://laravel.com/docs">Docs</a>
-        <a href="https://laracasts.com">Laracasts</a>
-        <a href="https://laravel-news.com">News</a>
-        <a href="https://blog.laravel.com">Blog</a>
-        <a href="https://nova.laravel.com">Nova</a>
-        <a href="https://forge.laravel.com">Forge</a>
-        <a href="https://github.com/laravel/laravel">GitHub</a>
-      </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 carousel-image" img src="images/book3.jpg" alt="Third slide">
+      <div class="carousel-caption d-none d-md-block">			    
+			    <p class="carousel-subtitle">Reading is the best gift of humanity</p>
+			    <h5 class="carousel-title">Keep Reading</h5>
+			    <button type="button" class="btn btn-dark text-light carousel-button">See more</button>
+			</div>
     </div>
   </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<!------------Jumbotron------------------------------------------------------->
+
+<div class="container-fluid search-bar py-3">
+	<div class="row" align="center">	
+    <div class="col-sm-12 col-md-3 col-lg-3 p-2">
+	  <input type="text" class="search-bar-elements text-secondary" placeholder="Book Title" aria-label="Recipient's username" aria-describedby="basic-addon2">
+		</div>	
+		<div class="dropdown col-sm-12 col-md-3 col-lg-3 p-2">
+		  <button class="btn btn-secondary dropdown-toggle search-bar-elements text-secondary d-block" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		   	Book Category
+		  </button>
+		  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+		    <a class="dropdown-item" href="#">Action</a>
+		    <a class="dropdown-item" href="#">Another action</a>
+		    <a class="dropdown-item" href="#">Something else here</a>
+		  </div>
+		</div>
+		<div class="dropdown col-sm-12 col-md-3 col-lg-3 p-2">
+		  <button class="btn btn-secondary dropdown-toggle search-bar-elements text-secondary d-block" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		   	Book Author
+		  </button>
+		  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+		    <a class="dropdown-item" href="#">Action</a>
+		    <a class="dropdown-item" href="#">Another action</a>
+		    <a class="dropdown-item" href="#">Something else here</a>
+		  </div>
+		</div>
+		<div class="col-sm-12 col-md-3 col-lg-3 p-2">
+			<button type="button" class="btn btn-dark search-bar-find-button text-light d-block">FIND BOOK</button>
+		</div>
+	</div>
+</div>
+
+
+<!-----------------------popular books------------------------------------------->
+<div class="container-fluid">
+	<div class="row title-display mt-3">
+	  <div class="col">Popular Books</div>
+	</div>
+	<div class="row popular-books-display pb-5">		
+	  <div class="col-sm-12 col-md-6 col-lg-3 p-md-1 p-sm-2">
+	  	<div class="popular-books-container pb-5">
+		  	<img src="images/popular1.jpg" class="popular-books-image mt-2">
+		  	<p class="popular-books-name">Destroy me</p>
+		  	<i class="popular-books-author">Taherah Mafi</i>
+		  	<p class="popular-books-price">$2.00</p>
+		  	<button type="button" class="btn btn-dark text-light">Detail</button>
+		  	<button type="button" class="btn btn-dark text-light">Buy</button>		 
+	  	</div> 
+	  </div>
+	 	<div class="col-sm-12 col-md-6 col-lg-3 p-md-1 p-sm-2">	  
+	 		<div class="popular-books-container pb-5">
+		  	<img src="images/popular2.jpg" class="popular-books-image mt-2">
+		  	<p class="popular-books-name">Soul</p>
+		  	<i class="popular-books-author">Whoever</i>
+		  	<p class="popular-books-price">$23.00</p>
+		  	<button type="button" class="btn btn-dark text-light">Detail</button>
+		  	<button type="button" class="btn btn-dark text-light">Buy</button>
+		  </div>
+	  </div>
+	  <div class="col-sm-12 col-md-6 col-lg-3 p-md-1 p-sm-2">
+	  	<div class="popular-books-container pb-5"> 	
+		  	<img src="images/popular3.jpg" class="popular-books-image mt-2">
+		  	<p class="popular-books-name">Sita</p>
+		  	<i class="popular-books-author">Amish</i>
+		  	<p class="popular-books-price">$32.00</p>
+		  	<button type="button" class="btn btn-dark text-light">Detail</button>
+		  	<button type="button" class="btn btn-dark text-light">Buy</button>
+		  </div>
+	  </div>
+	  <div class="col-sm-12 col-md-6 col-lg-3 p-md-1 p-sm-2">
+	  	<div class="popular-books-container pb-5">
+		  	<img src="images/popular4.jpg" class="popular-books-image mt-2	">
+		  	<p class="popular-books-name">Soaring</p>
+		  	<i class="popular-books-author">Kristen Ashley</i>
+		  	<p class="popular-books-price">$12.00</p>
+		  	<button type="button" class="btn btn-dark text-light">Detail</button>
+		  	<button type="button" class="btn btn-dark text-light">Buy</button>
+		  </div>
+	  </div>		
+	</div>
+</div>
+<!-----------------------featured book------------------------------------------->
+<div class="container-fluid">
+	<div class="row">
+	  <div class="offset-lg-1 col-12 col-md-12 col-lg-3 my-auto pb-5 py-5 featured-books-description-col">
+	  	<p class="featured-book-word">Featured Book</p>
+	  	<p class="featured-book-title">The complete idiots guide to graphic design</p>
+	  	<p class="featured-book-author">Anggi Krisna</p>
+	  	<p class="featured-book-description">From advanced selectors to generated content to web fonts, and from gradients, shadows, and rounded corners to elegant animations, CSS3 hold a universe of creative possibilities. No one can better guide you through these galaxies than Dan Cederholm.</p>
+	  	<button type="button" class="btn btn-dark text-light">Get this book</button>
+	  </div>
+	  <div class="col-12 col-md-6 col-lg-4 pb-5 py-5 my-auto" align="center">
+	  	<img src="images/popular4.jpg" class="featured-book-image">
+	  </div>
+	  <div class="col-12 col-md-6 col-lg-4 pb-5 py-5 my-auto" align="center">
+	  	<img src="images/popular2.jpg" class="featured-book-image">
+	  </div>
+	</div>
+</div>
+
+<!-----------------------popular courses------------------------------------------->
+<div class="container-fluid">
+	<div class="row popular-course-title-display">
+	  <div class="col">Popular Courses</div>
+	</div>
+	<div class="row popular-courses-display" align="center">  
+	  <div class="col-sm-12 col-md-6 col-lg-3 p-md-1 p-sm-2">	 
+	  	<div class="popular-courses-container pb-5">
+		  	<img src="images/popular1.jpg" class="popular-courses-image mt-2">
+		  	<p class="popular-courses-name d-inline">Destroy me</p>		  	
+		  	<button type="button" class="btn btn-dark text-light d-inline">Buy</button>
+		  </div>
+	  </div>
+	 	<div class="col-sm-12 col-md-6 col-lg-3 p-md-1 p-sm-2">
+	 		<div class="popular-courses-container pb-5">
+		  	<img src="images/popular2.jpg" class="popular-courses-image mt-2">
+		  	<p class="popular-courses-name d-inline">Soul</p>		  	
+		  	<button type="button" class="btn btn-dark text-light d-inline">Buy</button>
+		  </div>
+	  </div>
+	  <div class="col-sm-12 col-md-6 col-lg-3 p-md-1 p-sm-2">
+	  	<div class="popular-courses-container pb-5">
+		  	<img src="images/popular3.jpg" class="popular-courses-image mt-2">
+		  	<p class="popular-courses-name d-inline">Sita</p>		  	
+		  	<button type="button" class="btn btn-dark text-light d-inline">Buy</button>
+		  </div>
+	  </div>
+	  <div class="col-sm-12 col-md-6 col-lg-3 p-md-1 p-sm-2">
+	  	<div class="popular-courses-container pb-5">
+		  	<img src="images/popular4.jpg" class="popular-courses-image mt-2">
+		  	<p class="popular-courses-name d-inline">Soaring</p>		  	
+		  	<button type="button" class="btn btn-dark text-light d-inline">Buy</button>
+		  </div>
+	  </div>		
+	</div>
+</div>
+<!-----------------------browse collection------------------------------------------->
+
+<div class="container-fluid">
+	<img src="images/bookstore.jpg" class="browse-collection-image">
+@endsection
